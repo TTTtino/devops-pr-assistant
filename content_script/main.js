282628418572271910b86cfe142a79d5d9483bd0
Tino Tom <Tino.Tom@streets-heaver.com>
2025-03-12 17:45:10 +0000

    feat: ctrl+slash to strikethrough line

--- content_script/main.js
+++ content_script/main.js
@@ -19,6 +19,40 @@ window.addEventListener("input", function (event) {
   }
 });
 
+window.addEventListener("keyup", function (e) {
+  if (e.target.tagName.toLowerCase() !== "input" && e.target.tagName.toLowerCase() !== "textarea") return;
+
+  // Strikethrough the current line or the current checklist item
+  if (e.ctrlKey && e.code === "Slash") {
+    let str = e.target.value;
+    const index = e.target.selectionStart;
+    const left = str.lastIndexOf("\n", index - 1);
+    const leftCR = str.lastIndexOf("\r\n", index - 2);
+    const leftCB = str.lastIndexOf("- [ ] ", index - 6);
+    const right = str.indexOf("\n", index);
+    const rightCR = str.indexOf("\r\n", index);
+
+    const start = ((leftCR !== -1 && leftCB > leftCR) || (left !== -1 && leftCB > left)) ? leftCB + 5 : ( leftCR !== -1 ? leftCR : (left === -1 ? 0 : left))
+    const end = rightCR !== -1 ? rightCR : (right === -1 ? str.length : right);
+
+    const newVal = [str.slice(0, start + 1), '~~', str.slice(start + 1, end), '~~', str.slice(end)].join('');
+
+
+    e.target.value = newVal;
+    const event = new InputEvent("input", {
+      data: newVal,
+      bubbles: true,
+    });
+    e.target.dispatchEvent(event);
+
+    let newIndex = index + 2;
+    if (index === start ) newIndex = index;
+    e.target.setSelectionRange(newIndex, newIndex);
+  }
+
+});
+
+
 function enterTextIntoFocusedTextbox(text, replace) {
   const focusedElement = document.activeElement;
   const caratPos = focusedElement.selectionStart;
